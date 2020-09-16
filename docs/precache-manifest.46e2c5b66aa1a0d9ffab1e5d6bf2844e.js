983d3957e29f9464acabadc868e20e1c8817833d
Rousan Ali <rousanali786@gmail.com>
2020-09-16 13:12:24 +0530

    Update

---
+++ docs/precache-manifest.46e2c5b66aa1a0d9ffab1e5d6bf2844e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1e56d533e08f7372300f4868cb1a6c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ff0faa04adb26e172df",
+    "url": "/static/css/2.edb8eddf.chunk.css"
+  },
+  {
+    "revision": "78672d8ea38e92674c8f",
+    "url": "/static/css/main.7b148cd4.chunk.css"
+  },
+  {
+    "revision": "3ff0faa04adb26e172df",
+    "url": "/static/js/2.246e158e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.246e158e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78672d8ea38e92674c8f",
+    "url": "/static/js/main.ac51a85b.chunk.js"
+  },
+  {
+    "revision": "ba126735810163618457",
+    "url": "/static/js/runtime-main.3374ac37.js"
+  }
+]);
